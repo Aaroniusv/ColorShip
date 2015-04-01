f1312056922a37586b97331599a47c0ba2bdd8ac
unknown <akummer@uim-ws-srq-1765.UICNA.local>
2015-04-01 14:51:48 -0400

    changes to repo made

--- main.js
+++ main.js
@@ -15,7 +15,32 @@ window.addEventListener("keyup", function(e) {
 
 });
 
-
+var Enemy =
+{
+  x: this.x || HEIGHT / 2,
+  y: this.y || WIDTH / 2,
+  radius: 20,
+  DrawCircle: function()
+  {
+    var color = Math.floor(Math.random() * 3) + 1
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+    if (color = 1)
+    {
+      ctx.fillStyle = 'green';
+    }if (color = 2)
+    {
+      ctx.fillStyle = 'red';
+    }if (color = 3)
+    {
+      ctx.fillStyle = 'purple';
+    }
+    ctx.fill();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#003300';
+    ctx.stroke();
+  },
+};
 
 var Player =
 {
@@ -28,16 +53,41 @@ var Player =
 	{
 		ctx.drawImage(Ship1,Player.x,Player.y,30,30);
 	},
+  Collider: function(laser,enemy)
+  {
+     if (Enemy.x - Player.x <= 40 && Player.x - Enemy.x <=40 )
+     {
+       return true;
+     }
+     else
+     {
+       return false;
+     }
+  },
   ShootLaser: function(color)
   {
     if (color == "red" && Player.LaserPower > 0 && Player.isShooting)
     {
-      ctx.beginPath();
-      ctx.moveTo(Player.x+15,Player.y);
-      ctx.lineTo(Player.x+15,0);
-      ctx.strokeStyle = 'red';
-      ctx.lineWidth = 3;
-      ctx.stroke();
+
+      if (Player.Collider == false)
+      {
+        ctx.beginPath();
+        ctx.moveTo(Player.x+15,Player.y);
+        ctx.lineTo(Player.x+15,0);
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 3;
+        ctx.stroke();
+      }
+      else if (Player.Collider)
+      {
+          ctx.beginPath();
+          ctx.moveTo(Player.x+15,Player.y);
+          ctx.lineTo(Player.x+15,Enemy.y);
+          ctx.strokeStyle = 'red';
+          ctx.lineWidth = 3;
+          ctx.stroke();
+      }
+
 
     }
      else if (color == "green" && Player.LaserPower > 0 && Player.isShooting)
@@ -59,7 +109,6 @@ var Player =
    }
     else
     {
-      //console.log("no color is selected or key is up");
     }
   },
 
@@ -127,11 +176,10 @@ render = function()
 	update(null);
   canvasHelper.drawMap();
   Player.DrawPlayer();
-
+  //Enemy.y += .3;
+  Enemy.DrawCircle();
   Player.ShootLaser(Player.LaserColor);
   //Player.LaserColor = null;
-
-
   //console.log(Player.LaserPower);
   requestAnimationFrame(render);
 }
